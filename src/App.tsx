8914616413d6d3b13f093e9aeb46507bfaf8885d
Miloš Stojanović <stojanovic.milos1999@gmail.com>
2023-07-27 10:11:36 +0200

    Initialize cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { useCookies } from 'react-cookie';
+
 
 function App() {
+
+  const [cookies, setCookie, removeCookie] = useCookies(['list_groups_with_tasks']);
+
+  // call on page load
+  function checkCookie() {
+    const cookie_present = cookies.list_groups_with_tasks;
+    if (cookie_present){
+      return cookies.list_groups_with_tasks;
+    }
+    else {
+      return "Alfred";
+    }
+  }
+
+  useEffect(() => {
+    alert(checkCookie())
+    setCookie('list_groups_with_tasks', checkCookie());
+  }, [cookies]) 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <div style={{minHeight: "1000px", backgroundColor: "seagreen"}}></div>
+        <Footer />
     </div>
   );
 }
