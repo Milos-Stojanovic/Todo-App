478373e669ffbffafbd6738bf808bdc74609bead
Miloš Stojanović <stojanovic.milos1999@gmail.com>
2023-08-02 13:46:31 +0200

    Edit appearance & add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import { useCookies } from 'react-cookie';
+import supabase from './config/supabaseClient';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Success from './components/Success';
+import Home from './components/Home';
 
 
 function App() {
 
   const [cookies, setCookie, removeCookie] = useCookies(['list_groups_with_tasks']);
+  const [user, setUser] = useState<any>(null)
+  const navigate = useNavigate();
+
+    supabase.auth.onAuthStateChange(async (event) => {
+        console.log(event)
+        if (event === "SIGNED_IN" /*&& event !== 'INITIAL_SESSION'*/){
+            navigate("/success");
+        }
+        else
+        if (event !== "INITIAL_SESSION"){
+            navigate("/")
+        }
+    })
+
 
   // call on page load
   function checkCookie() {
@@ -22,15 +42,18 @@ function App() {
   }
 
   useEffect(() => {
-    alert(checkCookie())
+    //alert(checkCookie())
     setCookie('list_groups_with_tasks', checkCookie());
-  }, [cookies]) 
+  }, [cookies, user]) 
 
 
   return (
     <div className="App">
-        <Header />
-        <div style={{minHeight: "1000px", backgroundColor: "seagreen"}}></div>
+        <Header  user={user} setUser={setUser}/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/success' element={<Success user={user} setUser={setUser} />} />
+        </Routes>
         <Footer />
     </div>
   );
