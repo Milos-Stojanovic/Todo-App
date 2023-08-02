25bd9c6780f2d02f5b5478a488632c874fc471ce
Miloš Stojanović <stojanovic.milos1999@gmail.com>
2023-08-02 15:40:31 +0200

    Add useGetUserData to userRepository

--- src/repositories/userRepository.tsx
+++ src/repositories/userRepository.tsx
@@ -1,10 +1,16 @@
+import { useEffect, useState } from "react";
 import supabase from "../config/supabaseClient"
 
-export async function getUserData() {
-    await supabase.auth.getUser().then(value => {
-        if(value.data?.user){
-            console.log(value.data.user)
-            return value.data.user;
-        }
-    })
-}
+
+export function useGetUserData() {
+    const[user, setUser] = useState<any>();
+
+    useEffect(() => {
+        supabase.auth.getUser().then((response) => {
+            const userData = response.data.user as any;
+            setUser(userData);
+        });
+    }, [])
+        
+    return user;
+  }
